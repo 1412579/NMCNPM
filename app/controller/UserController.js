6aff8b12e4e26aa03c777a947913bbbef972127d
NguyenThang <nguyenthang1597@gmail.com>
2017-11-15 19:21:02 +0700

    update user

--- app/controller/UserController.js
+++ app/controller/UserController.js
@@ -3,19 +3,45 @@
 
 var express = require('express');
 var router = express.Router();
-
+var Users = require("../model/user");
 
 router.get('/list', function(req, res) {
-    res.render('admin/user/list',{
-        layout: 'main-admin',
-    });
+    Users.getAllUser()
+    .then(result => {
+        res.render('admin/user/list', {
+            layout: 'main-admin',
+            list: result,
+        })
+    })
 });
 
-router.get('/edit', function(req, res) {
-    res.render('admin/user/edit',{
-        layout: 'main-admin',
-    });
+router.get('/edit/:id', function(req, res) {
+    Users.getUserById(req.params.id)
+    .then(result => {
+        res.render('admin/user/edit',{
+            layout: 'main-admin',
+            user_: result,
+        });
+    })
 });
+
+router.post('/edit/', function(req, res){
+    var tmp = !req.body.checkbox ? "TRUE" : "FALSE";
+    var user = {
+        id: req.body.id,
+        email: req.body.email,
+        fullname: req.body.fullname,
+        phone: req.body.phone,
+        role_id: req.body.role,
+        active: tmp
+    }
+    Users.updateByID(user)
+    .then(function(){
+        req.flash('messageCate', 'Đã sửa người dùng thành công!');
+        res.redirect("/admin/user/list");
+    })
+    
+})
 
 module.exports = router;
 
--- app/model/user.js
+++ app/model/user.js
@@ -1,8 +1,7 @@
 const pool = require('../model/pg');
 var User = {
-	getUserById: function(user) {
-  var sql = `select * from users where id=${user.id}`;
-  console.log(sql);
+	getUserById: function(id) {
+  var sql = 'select * from users where id=' + id;
     return new Promise(function(resolve, reject) {
       pool.query(sql, function(errors, result) {
         if(errors) {
@@ -13,6 +12,28 @@ var User = {
       });
     });
   },
+  getAllUser: function(){
+    var sql = 'select users.*, role.name from users, role where users.role_id = role.id';
+    return new Promise(function(resolve,reject){
+      pool.query(sql, function(errors, result){
+        if(errors)
+          reject(errors);
+        else
+          resolve(result.rows);
+      })
+    })
+  },
+  updateByID: function(user){
+    return new Promise(function(resolve, reject){
+      var query = `update users set email = '${user.email}', fullname = '${user.fullname}', phone = '${user.phone}', role_id = ${user.role_id}, active = ${user.active} where id = ${user.id}`
+      pool.query(query, function(errors, result){
+        if(errors)
+          reject(errors);
+        else
+          resolve(result);
+      })
+    })
+  }
 }
 
 module.exports = User;
